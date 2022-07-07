d434df6e60317d573d5a1d2ce61e6471e6c3991b
timbaron <timothy.akiode@student.aul.edu.ng>
2022-07-07 18:20:22 +0100

    Update users table, add full name

---
+++ database/migrations/2022_07_07_171521_update_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->string('first_name')->after('id');
+            $table->string('last_name')->after('first_name');
+            $table->string('full_name')->virtualAs("CONCAT(first_name, ' ', last_name)")->after('last_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+           //
+        });
+    }
+};
